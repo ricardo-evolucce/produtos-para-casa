69df3dbf4eb5913ae912be69b387ecf61d399a0c
ricardo <ricardo.evolucce@gmail.com>
2025-08-14 15:43:13 -0300

    ajuste sitemap

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -3,9 +3,45 @@
     @foreach($urls as $url)
     <url>
         <loc>{{ $url['loc'] }}</loc>
-        <lastmod>{{ $url['lastmod'] }}</lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.8</priority>
+        <lastmod>{{ \Carbon\Carbon::parse($url['lastmod'])->format('Y-m-d') }}</lastmod>
+        <changefreq>
+            @if(isset($url['type']))
+                @switch($url['type'])
+                    @case('homepage')
+                        daily
+                        @break
+                    @case('category')
+                        weekly
+                        @break
+                    @case('product')
+                        weekly
+                        @break
+                    @default
+                        monthly
+                @endswitch
+            @else
+                weekly
+            @endif
+        </changefreq>
+        <priority>
+            @if(isset($url['type']))
+                @switch($url['type'])
+                    @case('homepage')
+                        1.0
+                        @break
+                    @case('category')
+                        0.8
+                        @break
+                    @case('product')
+                        0.5
+                        @break
+                    @default
+                        0.5
+                @endswitch
+            @else
+                0.8
+            @endif
+        </priority>
     </url>
     @endforeach
 </urlset>
