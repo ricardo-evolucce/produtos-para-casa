21b822497201055f796c05a6badc6e8bf5af6391
ricardo <ricardo.evolucce@gmail.com>
2025-08-14 15:52:18 -0300

    ajuste sitemap

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,15 +1,17 @@
 {!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
-<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:schemaLocation="https://www.sitemaps.org/schemas/sitemap/0.9 https://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
     @foreach($urls as $url)
     <url>
-        <loc>{{ $url['loc'] }}</loc>
+        <loc>{{ e($url['loc']) }}</loc>
         <lastmod>{{ \Carbon\Carbon::parse($url['lastmod'])->format('Y-m-d') }}</lastmod>
         <changefreq>
             @php
-                // Definindo frequência de atualização automática
-                if(str_contains($url['loc'], '/inicio')) {
+                $loc = $url['loc'];
+                if (str_contains($loc, '/inicio')) {
                     echo 'daily';
-                } elseif(str_contains($url['loc'], '/produtos-para-')) {
+                } elseif (str_contains($loc, '/produtos-para-')) {
                     echo 'weekly';
                 } else {
                     echo 'monthly';
@@ -18,10 +20,10 @@
         </changefreq>
         <priority>
             @php
-                // Definindo prioridade automática
-                if(str_contains($url['loc'], '/inicio')) {
+                $loc = $url['loc'];
+                if (str_contains($loc, '/inicio')) {
                     echo '1.0';
-                } elseif(str_contains($url['loc'], '/produtos-para-')) {
+                } elseif (str_contains($loc, '/produtos-para-')) {
                     echo '0.8';
                 } else {
                     echo '0.5';
